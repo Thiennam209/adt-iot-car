91a8646c15b249afd678dea2b4ffde4f065bd5c1
Nguyen Duong Huy <ndhuy2711>
2023-05-08 16:19:48 +0700

    update zip

--- functions/telemetryfunction.cs
+++ functions/telemetryfunction.cs
@@ -42,7 +42,7 @@ namespace My.Function
                    string deviceId = (string)alertMessage["systemProperties"]["iothub-connection-device-id"];
                    var ID = alertMessage["body"]["deviceid"];
                    //var timeinterval = alertMessage["body"]["timeinterval"];
-                   var humidity = alertMessage["body"]["humidity"];
+                //    var humidity = alertMessage["body"]["humidity"];
                     //var temperature = alertMessage["body"]["temperature"];
                     //var pressure = alertMessage["body"]["pressure"];
                     //var magnetometerX = alertMessage["body"]["magnetometerX"];
@@ -56,7 +56,7 @@ namespace My.Function
                     //var gyroscopeZ = alertMessage["body"]["gyroscopeZ"];
                     log.LogInformation($"Device:{deviceId} Device Id is:{ID}");
                     //log.LogInformation($"Device:{deviceId} Device Id is:{timeinterval}");
-                    log.LogInformation($"Device:{deviceId} Device Id is:{humidity}");
+                    // log.LogInformation($"Device:{deviceId} Device Id is:{humidity}");
                     //log.LogInformation($"Device:{deviceId} Device Id is:{temperature}");
                     //log.LogInformation($"Device:{deviceId} Device Id is:{pressure}");
                     //log.LogInformation($"Device:{deviceId} Device Id is:{magnetometerX}");
@@ -89,51 +89,51 @@ namespace My.Function
                     JObject deviceMessage = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
                     string deviceId = (string)deviceMessage["systemProperties"]["iothub-connection-device-id"];
                     var ID = deviceMessage["body"]["deviceid"];
-                    var timeinterval = deviceMessage["body"]["timeinterval"];
-                    var humidity = deviceMessage["body"]["humidity"];
-                    var temperature = deviceMessage["body"]["temperature"];
-                    var pressure = deviceMessage["body"]["pressure"];
-                    var magnetometerX = deviceMessage["body"]["magnetometerX"];
-                    var magnetometerY = deviceMessage["body"]["magnetometerY"];
-                    var magnetometerZ = deviceMessage["body"]["magnetometerZ"];
-                    var accelerometerX = deviceMessage["body"]["accelerometerX"];
-                    var accelerometerY = deviceMessage["body"]["accelerometerY"];
-                    var accelerometerZ = deviceMessage["body"]["accelerometerZ"];
-                    var gyroscopeX = deviceMessage["body"]["gyroscopeX"];
-                    var gyroscopeY = deviceMessage["body"]["gyroscopeY"];
-                    var gyroscopeZ = deviceMessage["body"]["gyroscopeZ"];
+                    // var timeinterval = deviceMessage["body"]["timeinterval"];
+                    // var humidity = deviceMessage["body"]["humidity"];
+                    // var temperature = deviceMessage["body"]["temperature"];
+                    // var pressure = deviceMessage["body"]["pressure"];
+                    // var magnetometerX = deviceMessage["body"]["magnetometerX"];
+                    // var magnetometerY = deviceMessage["body"]["magnetometerY"];
+                    // var magnetometerZ = deviceMessage["body"]["magnetometerZ"];
+                    // var accelerometerX = deviceMessage["body"]["accelerometerX"];
+                    // var accelerometerY = deviceMessage["body"]["accelerometerY"];
+                    // var accelerometerZ = deviceMessage["body"]["accelerometerZ"];
+                    // var gyroscopeX = deviceMessage["body"]["gyroscopeX"];
+                    // var gyroscopeY = deviceMessage["body"]["gyroscopeY"];
+                    // var gyroscopeZ = deviceMessage["body"]["gyroscopeZ"];
 
-                    log.LogInformation($"Device:{deviceId} Device Id is:{ID}");
-                    log.LogInformation($"Device:{deviceId} Time interval is:{timeinterval}");
-                    log.LogInformation($"Device:{deviceId} humidity is:{humidity}");
-                    log.LogInformation($"Device:{deviceId} temperature is:{temperature}");
-                    log.LogInformation($"Device:{deviceId} pressure is:{pressure}");
-                    log.LogInformation($"Device:{deviceId} magnetometerX is:{magnetometerX}");
-                    log.LogInformation($"Device:{deviceId} magnetometerY is:{magnetometerY}");
-                    log.LogInformation($"Device:{deviceId} magnetometerZ is:{magnetometerZ}");
-                    log.LogInformation($"Device:{deviceId} accelerometerX is:{accelerometerX}");
-                    log.LogInformation($"Device:{deviceId} accelerometerY is:{accelerometerY}");
-                    log.LogInformation($"Device:{deviceId} accelerometerZ is:{accelerometerZ}");
-                    log.LogInformation($"Device:{deviceId} gyroscopeX is:{gyroscopeX}");
-                    log.LogInformation($"Device:{deviceId} gyroscopeY is:{gyroscopeY}");
-                    log.LogInformation($"Device:{deviceId} gyroscopeZ is:{gyroscopeZ}");
+                    // log.LogInformation($"Device:{deviceId} Device Id is:{ID}");
+                    // log.LogInformation($"Device:{deviceId} Time interval is:{timeinterval}");
+                    // log.LogInformation($"Device:{deviceId} humidity is:{humidity}");
+                    // log.LogInformation($"Device:{deviceId} temperature is:{temperature}");
+                    // log.LogInformation($"Device:{deviceId} pressure is:{pressure}");
+                    // log.LogInformation($"Device:{deviceId} magnetometerX is:{magnetometerX}");
+                    // log.LogInformation($"Device:{deviceId} magnetometerY is:{magnetometerY}");
+                    // log.LogInformation($"Device:{deviceId} magnetometerZ is:{magnetometerZ}");
+                    // log.LogInformation($"Device:{deviceId} accelerometerX is:{accelerometerX}");
+                    // log.LogInformation($"Device:{deviceId} accelerometerY is:{accelerometerY}");
+                    // log.LogInformation($"Device:{deviceId} accelerometerZ is:{accelerometerZ}");
+                    // log.LogInformation($"Device:{deviceId} gyroscopeX is:{gyroscopeX}");
+                    // log.LogInformation($"Device:{deviceId} gyroscopeY is:{gyroscopeY}");
+                    // log.LogInformation($"Device:{deviceId} gyroscopeZ is:{gyroscopeZ}");
                     var updateProperty = new JsonPatchDocument();
                     var turbineTelemetry = new Dictionary<string, Object>()
                     {
                         ["deviceid"] = ID,
-                        ["timeinterval"] = timeinterval,
-                        ["humidity"] = humidity,
-                        ["temperature"] = temperature,
-                        ["pressure"] = pressure,
-                        ["magnetometerX"] = magnetometerX,
-                        ["magnetometerY"] = magnetometerY,
-                        ["magnetometerZ"] = magnetometerZ,
-                        ["accelerometerX"] = accelerometerX,
-                        ["accelerometerY"] = accelerometerY,
-                        ["accelerometerZ"] = accelerometerZ,
-                        ["gyroscopeX"] = gyroscopeX,
-                        ["gyroscopeY"] = gyroscopeY,
-                        ["gyroscopeZ"] = gyroscopeZ
+                        // ["timeinterval"] = timeinterval,
+                        // ["humidity"] = humidity,
+                        // ["temperature"] = temperature,
+                        // ["pressure"] = pressure,
+                        // ["magnetometerX"] = magnetometerX,
+                        // ["magnetometerY"] = magnetometerY,
+                        // ["magnetometerZ"] = magnetometerZ,
+                        // ["accelerometerX"] = accelerometerX,
+                        // ["accelerometerY"] = accelerometerY,
+                        // ["accelerometerZ"] = accelerometerZ,
+                        // ["gyroscopeX"] = gyroscopeX,
+                        // ["gyroscopeY"] = gyroscopeY,
+                        // ["gyroscopeZ"] = gyroscopeZ
                     };
                     updateProperty.AppendAdd("/deviceid", ID.Value<string>());
 
